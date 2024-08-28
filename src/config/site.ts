8468304e433eb3004d3dea14420ab13438bcce49
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2024-08-29 01:51:49 +0900

    fix: biome rules

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,12 +1,12 @@
 import type { SiteConfig } from '@/types/site'
 
 export const siteConfig: SiteConfig = {
-	name: 'Post Content App',
-	description: 'Post Content App',
-	url: 'https://localhost:3000',
-	ogImage: 'https://post-content-app.vercel.app/og-image.png',
-	links: {
-		x: 'https://x.com/shota_nakaji',
-		github: 'https://github.com/shouta-nakashima'
-	}
+  name: 'Post Content App',
+  description: 'Post Content App',
+  url: 'https://localhost:3000',
+  ogImage: 'https://post-content-app.vercel.app/og-image.png',
+  links: {
+    x: 'https://x.com/shota_nakaji',
+    github: 'https://github.com/shouta-nakashima'
+  }
 }
