375229a6c825140038ee19f1f036005a9cca9b12
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2024-08-29 14:03:13 +0900

    feat: create contact section

--- src/components/MobileNavigation/index.tsx
+++ src/components/MobileNavigation/index.tsx
@@ -1,12 +1,15 @@
 import { siteConfig } from '@/config/site'
 import type { NavItem } from '@/types/marketing'
+import { useLockBodyScroll } from '@uidotdev/usehooks'
 import NextLink from 'next/link'
 
 type Props = {
   items: NavItem[]
+  onClose: () => void
 }
 
-export const MobileNavigation = ({ items }: Props) => {
+export const MobileNavigation = ({ items, onClose }: Props) => {
+  useLockBodyScroll()
   return (
     <div className="fixed top-16 inset-0 z-50 p-6 shadow-md md:hidden animate-in slide-in-from-bottom-80">
       <div className="grid gap-6 bg-white text-popover-foreground p-4 shadow-md">
@@ -16,6 +19,7 @@ export const MobileNavigation = ({ items }: Props) => {
         <nav className="text-sm flex gap-4">
           {items.map((item, index) => (
             <NextLink
+              onClick={onClose}
               className="p-2 font-medium hover:underline"
               key={index}
               href={item.href}
