78d79781e96574473f0a4b6ee2b3b0213d5b0231
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2024-08-29 17:06:42 +0900

    feat: setup contentlayer

---
+++ contentlayer.config.ts
@@ -0,0 +1,38 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+
+const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: 'blog/**/*.mdx',
+  fields: {
+    title: {
+      type: 'string',
+      required: true
+    },
+    description: {
+      type: 'string'
+    },
+    date: {
+      type: 'string',
+      required: true
+    },
+    published: {
+      type: 'boolean',
+      required: true
+    },
+    image: {
+      type: 'string',
+      required: true
+    }
+  },
+  computedFields: {
+    slug: {
+      type: 'string',
+      resolve: (doc) => `${doc._raw.flattenedPath}`
+    }
+  }
+}))
+
+export default makeSource({
+  contentDirPath: './content',
+  documentTypes: [Post]
+})
