8011da21093535d77b512cba59cd9aeea92d54cb
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2024-08-31 16:39:10 +0900

    feat: google auth and register page

--- auth.ts
+++ auth.ts
@@ -1,11 +1,16 @@
 import NextAuth, { type NextAuthConfig } from 'next-auth'
 import Github from 'next-auth/providers/github'
+import Google from 'next-auth/providers/google'
 
 export const config: NextAuthConfig = {
   providers: [
     Github({
       clientId: process.env.AUTH_GITHUB_ID,
       clientSecret: process.env.AUTH_GITHUB_SECRET
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET
     })
   ],
   basePath: '/api/auth',
--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,7 +1,9 @@
+import { GoogleIcon } from '@/components/Icon/icons/Google'
 import { Loader2 } from 'lucide-react'
 import { GithubIcon } from './icons/Github'
 
 export const Icon = {
   spinner: Loader2,
-  github: ({ ...props }) => <GithubIcon {...props} />
+  github: ({ ...props }) => <GithubIcon {...props} />,
+  google: ({ ...props }) => <GoogleIcon {...props} />
 }
