eba31bd09ce105e3e90f00af6ae57128e086f50b
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2024-08-31 15:46:03 +0900

    fix: middleware with redirect

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,12 +1,4 @@
-import { redirect } from 'next/navigation'
-import { auth } from '../../../../auth'
-
-const RegisterPage = async () => {
-  const session = await auth()
-
-  if (session?.user) {
-    redirect('/dashboard')
-  }
+const RegisterPage = () => {
   return (
     <div>
       <h1>Register</h1>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,24 @@
-export { auth as middleware } from 'auth'
+import { auth as middleware } from 'auth'
+import { NextResponse } from 'next/server'
 
 // Or like this if you need to do something here.
-// export default auth((req) => {
-//   console.log(req.auth) //  { session: { user: { ... } } }
-// })
+export default middleware((req) => {
+  const token = req.auth?.user
+  if (token) {
+    const isAuthPage =
+      req.nextUrl.pathname.startsWith('/login') ||
+      req.nextUrl.pathname.startsWith('/register')
+
+    if (isAuthPage) {
+      return NextResponse.redirect(new URL('/dashboard', req.url))
+    }
+  } else {
+    const isRedirectTargetPage = req.nextUrl.pathname.startsWith('/dashboard')
+    if (isRedirectTargetPage) {
+      return NextResponse.redirect(new URL('/login', req.url))
+    }
+  }
+})
 
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
